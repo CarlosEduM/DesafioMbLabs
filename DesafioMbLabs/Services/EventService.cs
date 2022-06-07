a7a609edf63720947a4ac93f2afa7acb57f71cf8
Carlos Eduardo <carlos.eduardo@icarotech.com>
2022-06-07 13:12:01 -0300

    EventService implementado

--- DesafioMbLabs/Services/EventService.cs
+++ DesafioMbLabs/Services/EventService.cs
@@ -1,12 +1,56 @@
-﻿using System.Threading.Tasks;
+﻿using DesafioMbLabs.Data;
+using DesafioMbLabs.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DesafioMbLabs.Services
 {
     public class EventService : IEventService
     {
-        public Task AddEvent()
+        private readonly SqlServerContext _dbContext;
+
+        public EventService(SqlServerContext dbContext)
         {
-            throw new System.NotImplementedException();
+            _dbContext = dbContext;
+        }
+
+        public async Task AddEvent(Event newEnvent)
+        {
+            _dbContext.Events.Add(newEnvent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public Event GetEvent(int id)
+        {
+            return _dbContext.Events.FirstOrDefault(e => e.Id == id);
+        }
+
+        public List<Event> GetUserEvents(User user)
+        {
+            return _dbContext.Events.Where(e => e.Manager.Id == user.Id).ToList();
+        }
+
+        public async Task RemoveEvent(Event eventToRemove)
+        {
+            _dbContext.Events.Remove(eventToRemove);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public List<Event> SearchForAEvent(string? eventname)
+        {
+            if (string.IsNullOrEmpty(eventname))
+                return _dbContext.Events.ToList();
+
+            return _dbContext.Events.Where(e => e.Name.Contains(eventname)).ToList();
+        }
+
+        public async Task UpdateEvent(Event newEvent)
+        {
+            _dbContext.Events.Update(newEvent).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
--- DesafioMbLabs/Services/IEventService.cs
+++ DesafioMbLabs/Services/IEventService.cs
@@ -1,9 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using DesafioMbLabs.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DesafioMbLabs.Services
 {
     public interface IEventService
     {
-        public Task AddEvent();
+        public Task AddEvent(Event newEvent);
+
+        public Task RemoveEvent(Event eventToRemove);
+
+        public Task UpdateEvent(Event newEvent);
+
+        public Event GetEvent(int id);
+
+        public List<Event> GetUserEvents(User user);
+
+        public List<Event> SearchForAEvent(string eventname);
     }
 }
